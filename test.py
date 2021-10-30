b477543ba4ce5f834f72b26c5e1d99ef3bb96745
Dan Kinsella <dan.kinsella@outlook.com>
2021-10-30 09:15:31 +0100

    create json file to be used with hmy cli

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import json
 import subprocess
 
+# Using the result of a hmyGetDelegatorsByValidator rpc request, work out value of missed block rewards
+# and create a json file to be used with hmy CLI for sending transactions: https://github.com/harmony-one/go-sdk
+
+passphrase_file = './passphrase.txt' # File containing the wallet passphrase
+working_directory = '~/harmony/'
+validator_address = 'one1w7nvheulzwprf9d9a3r8sqtv5q47qlqx7kured' # address to send rewards from
+
 file_name = "data.json"
 with open(file_name) as f:
     data = json.load(f)
@@ -10,7 +17,7 @@ total_stake = 0
 total_reward = 0
 hrs_not_signing = 18.2
 blocks_missed = 133376
-list_of_payouts = list()
+transactions = list()
 apr = 0.1
 seconds_per_year = 365 * 24 * 60 * 60
 blocks_per_second = 0.5
@@ -19,27 +26,31 @@ rewards_multiplier = (apr / seconds_per_year) * blocks_missed * blocks_per_secon
 for delegator in data['result']:
     #if delegator['delegator_address'] == 'one1w7nvheulzwprf9d9a3r8sqtv5q47qlqx7kured' :
     stake = delegator['amount'] / 1000000000000000000
-    if stake > 99 :
+    if stake > 99 and delegator['delegator_address'] != 'one1w7nvheulzwprf9d9a3r8sqtv5q47qlqx7kured'  :
         total_stake += stake
         reward = stake * rewards_multiplier
         total_reward += reward
         count += 1
-        payout = {
-            "address": delegator['delegator_address'],
-            "stake": stake,
-            "reward": reward
+        transaction = {
+            "from": validator_address,
+            "to": delegator['delegator_address'],
+            "from-shard" : "0",
+            "to-shard": "0",
+            "amount": reward,
+            "passphrase-file": passphrase_file
         }
-        list_of_payouts.append(payout)
+        transactions.append(transaction)
 
-list_of_payouts.sort(key=lambda payout: payout.get('reward')) 
+transactions.sort(key=lambda transaction: transaction.get('amount')) 
 
-for payout in list_of_payouts:
-    print(payout['address'], payout['reward'])
+#for transaction in transactions:
+#    print(transaction)
+
+with open("./rewards-epoch-test.json", "w") as file:
+    json.dump([ob for ob in transactions], file)
 
 
 print('Total Stake', total_stake)
 print('Total Reward', total_reward)
 print('Delegators', count)
 
-# to-do use suprocess to call hmy send transaction
-#subprocess.run(['notepad'])
